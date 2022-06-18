40922f1b948339653f2dbc48e2b7258dd34bb118
Fabio Roberto <fabiorvs@globo.com>
2022-06-18 18:53:40 -0300

    Updates

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'fabio',
+    password: '123',
+    database: 'lms',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  };
+  
